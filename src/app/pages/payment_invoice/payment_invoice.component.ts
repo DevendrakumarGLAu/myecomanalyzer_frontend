123bb28db3a6d795ca014c6409dcae3523917651
Devendra kumar <devendra1997kumar@gmail.com>
2026-04-24 09:53:45 +0530

    add assest and dashboard correct 222

--- src/app/pages/payment_invoice/payment_invoice.component.ts
+++ src/app/pages/payment_invoice/payment_invoice.component.ts
@@ -10,23 +10,27 @@ import { PLATFORMS } from '../../common/constant/platform.constants';
   standalone: true,
   imports: [NgFor, FormsModule, CommonModule],
   templateUrl: './payment_invoice.component.html',
-//   styleUrl: './invoice-upload.component.scss'
 })
 export class PaymentExcelComponent {
+
   selectedFile: File | null = null;
   selectedPlatform: string = '';
-  message: string = '';
+
   platforms = PLATFORMS;
 
+  message = '';
 
-  constructor(private http: HttpClient,
+  // ✅ RESPONSE STATE
+  responseData: any = null;
+  skippedDetails: any[] = [];
+
+  constructor(
+    private http: HttpClient,
     private invoiceService: InvoiceService
-  ) { }
+  ) {}
 
   onFileSelected(event: any) {
-
     const file = event.target.files[0];
-
     if (!file) return;
 
     const allowedTypes = [
@@ -44,29 +48,23 @@ export class PaymentExcelComponent {
   }
 
   uploadFile() {
-    console.log("Selected Platform:", this.selectedPlatform);
-  console.log("Selected File:", this.selectedFile);
     if (!this.selectedFile || !this.selectedPlatform) {
       this.message = "Please select platform and file.";
       return;
     }
 
-    const formData = new FormData();
-    formData.append('file', this.selectedFile);
-
     this.invoiceService.uploadExcel(this.selectedFile, this.selectedPlatform)
       .subscribe({
         next: (res: any) => {
-          const summary = res?.data?.summary;
-          this.message = `
-    ✅ Imported: ${summary.imported}
-    🔁 Duplicate (Not Inserted): ${summary.duplicates}
-    ❌ Error (Not Inserted): ${summary.errors}
-    ⚠️ Total Not Inserted: ${summary.not_inserted}
-  `;
+
+          // ✅ store full response
+          this.responseData = res;
+          this.skippedDetails = res?.skipped_details || [];
+
+          this.message = res?.message || "Upload completed";
         },
         error: (err: any) => {
-          this.message = 'Upload failed: ' + (err.error?.message || err.message);
+          this.message = 'Upload failed: ' + (err.error?.detail || err.message);
         }
       });
   }
